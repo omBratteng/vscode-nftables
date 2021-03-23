f0ddfa68f27d62f77daeee543d7a3bda75c9428b
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-03-23 15:02:20 +0100

    Add linting with prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,53 @@
-'use strict';
+'use strict'
 
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
+	vscode.languages.registerDocumentFormattingEditProvider(
+		{ scheme: 'file', language: 'nft' },
+		{
+			provideDocumentFormattingEdits(
+				document: vscode.TextDocument,
+			): vscode.TextEdit[] {
+				const edits: vscode.TextEdit[] = []
 
-    vscode.languages.registerDocumentFormattingEditProvider({ scheme: 'file', language: 'nft' }, {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+				let indentLevel = 0
 
-            let edits:vscode.TextEdit[] = [];
+				for (let i = 0; i < document.lineCount; i++) {
+					const line: string = document.lineAt(i).text.trim()
 
-            let indentLevel: number = 0;
+					let skip = false
 
-            for (var i = 0; i < document.lineCount; i++) {
-                const line: string = document.lineAt(i).text.trim();
+					if (line.includes('{') && line.includes('}')) {
+						skip = true
+					}
 
-                let skip: Boolean = false;
+					if (!skip && line.endsWith('}')) {
+						indentLevel--
+					}
 
-                if (line.includes("{") && line.includes("}")) {
-                    skip = true;
-                }
+					// Generate new indentated line
+					let newline: string = '\t'.repeat(indentLevel) + line
 
-                if (!skip && line.endsWith("}")) {
-                    indentLevel--;
-                }
+					if (newline.trim() == '') {
+						// No indentation for empty line
+						newline = ''
+					}
 
-                // Generate new indentated line
-                let newline:string = "\t".repeat(indentLevel) + line;
+					edits.push(
+						vscode.TextEdit.replace(
+							document.lineAt(i).range,
+							newline,
+						),
+					)
 
-                if (newline.trim() == "") {
-                    // No indentation for empty line
-                    newline = "";
-                }
+					if (!skip && line.endsWith('{')) {
+						indentLevel++
+					}
+				}
 
-                edits.push(
-                    vscode.TextEdit.replace(document.lineAt(i).range, newline)
-                );
-
-                if (!skip && line.endsWith("{")) {
-                    indentLevel++;
-                }
-
-            }
-
-            return edits;
-        }
-    });
+				return edits
+			},
+		},
+	)
 }
